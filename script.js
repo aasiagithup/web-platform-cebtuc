8613a442ec706e33a74c3473a68aa0491ed460b3
aasiagithup <132499880+aasiagithup@users.noreply.github.com>
2023-05-30 02:04:10 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+button.forEach(button => {
+  button.addEventListener('click',function(){
+    document.querySelector('#nums').textContent =button.inner.HTML
+  })
+  document.querySelector('#nums')
+  )
+})
